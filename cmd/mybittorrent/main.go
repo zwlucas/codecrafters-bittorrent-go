6091bea631659a2e5d152726f3581ca2d1bd235c
jet2tlf <jet2tlf@gmail.com>
2024-06-03 14:43:29 -0300

    codecrafters submit [skip ci]

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
 	"os"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
+	"strings"
+
+	bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
 func main() {
@@ -13,8 +16,7 @@ func main() {
 	switch command {
 	case "decode":
 		bencodedValue := os.Args[2]
-
-		decoded, err := NewDecoder(bencodedValue).Decode()
+		decoded, err := bencode.Decode(strings.NewReader(bencodedValue))
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -26,18 +28,20 @@ func main() {
 	case "info":
 		f, err := os.Open(os.Args[2])
 		if err != nil {
-			fmt.Println("Failed to open file: " + os.Args[2])
+			fmt.Println("Failed to open file: ", os.Args[2])
 		}
 
-		tf := NewFile()
-
-		err = tf.ReadFrom(f)
-		if err != nil {
-			fmt.Println("Failed to parse file: " + os.Args[2])
+		var meta Meta
+		if err = bencode.Unmarshal(f, &meta); err != nil {
 			panic(err)
 		}
 
-		fmt.Printf("Tracker URL: %s\nLength: %d", tf.Announce, tf.Info.Length)
+		sha := sha1.New()
+		if err = bencode.Marshal(sha, meta.Info); err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Tracker URL: %s\nLength: %d\nInfo Hash: %x", meta.Announce, meta.Info.Length, sha.Sum(nil))
 
 	default:
 		fmt.Println("Unknown command: " + command)
--- go.mod
+++ go.mod
@@ -10,4 +10,4 @@ module github.com/codecrafters-io/bittorrent-starter-go
 
 go 1.22
 
-require github.com/jackpal/bencode-go v1.0.0 // indirect
+require github.com/jackpal/bencode-go v1.0.2 // indirect
